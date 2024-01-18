7a0f642a1fcd9f304912fa3c7bd4d8a3f9545e75
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-18 16:30:48 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '~resources': '/resources/',
+            '~bootstrap' : resolve(__dirname, 'node_modules/bootstrap'),
+
+        }
+    }
+});
