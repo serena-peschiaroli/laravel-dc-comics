2174978eae8aabedcb4a92342e4173269d521558
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-22 15:50:40 +0100

    Add: handling old in edit view

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,35 +12,35 @@
                     {{-- inserire msg errori x ogni campo --}}
                     <div class="mb-3">
                         <label for="title" class="form-label">Title</label>
-                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ $comic->title }}">
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title', $comic->title) }}">
                         @error('title')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="mb-3">
                         <label for="thumb" class="form-label">Image</label>
-                        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ $comic->thumb }}">
+                        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb', $comic->thumb) }}">
                         @error('thumb')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="mb-3">
                         <label for="price" class="form-label">Price</label>
-                        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" step="0.01"value="{{ $comic->price }}">
+                        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" step="0.01"value="{{ old('price', $comic->price) }}">
                          @error('price')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Sale date</label>
-                        <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+                        <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
                         @error('sale_date')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="mb-3">
                         <label for="series" class="form-label">Series</label>
-                        <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ $comic->series }}">
+                        <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series', $comic->series) }}">
                         @error('series')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -48,8 +48,8 @@
                     <div class="mb-3">
                         <label for="type" class="form-label">Type</label>
                         <select id="type" class="form-select @error('type') is-invalid @enderror" name="type">
-                            <option @selected($comic->type === 'comic book') value="comic book">Comic Book</option>
-                            <option @selected($comic->type === 'graphic novel')  value="graphic novel">Graphic Novel</option>
+                            <option @selected(old('type', $comic->type) === 'comic book') value="comic book">Comic Book</option>
+                            <option @selected(old('type', $comic->type) === 'graphic novel')  value="graphic novel">Graphic Novel</option>
                         </select>
                         @error('type')
                             <div class="invalid-feedback">{{ $message }}</div>
